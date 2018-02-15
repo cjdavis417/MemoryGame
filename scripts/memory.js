c4043f851d24fec83dfc9e0863a8baddf2bb9be1
Christopher Davis <christopherjdavis417@gmail.com>
2018-02-15 14:24:16 -0500

    edits

---
+++ scripts/memory.js
@@ -0,0 +1,11 @@
+var numberOfButtons;
+
+$('button')
+
+function cardWidth(cardHeight)  {
+    var cardRatio = 63 / 88;
+    var cWidth;
+    return cdWidth = cardHeight * cardRatio;
+}
+
+$('#card1').height(500).width(cardWidth(500));
